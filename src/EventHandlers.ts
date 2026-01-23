257a11a05619d9183597b864ff2de34126cce52f
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2026-01-23 15:05:04 -0300

    adapt tables to match goldsky schema

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -3,10 +3,10 @@
  */
 import {
   AdminUpgradeabilityProxy,
-  Token,
-  Account,
-  Event,
-  Transfer,
+  poaps,
+  collectors,
+  drops_stats,
+  transfers,
 } from "generated";
 
 function createEventID(event: any): string {
@@ -24,47 +24,47 @@ function createAccountID(address: string, chainId: number): string {
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 AdminUpgradeabilityProxy.EventToken.handler(async ({ event, context }) => {
-  let ev = await context.Event.get(createEventID(event));
+  let ev = await context.drops_stats.get(createEventID(event));
   // This handler always run after the transfer handler
-  let token = (await context.Token.get(
+  let poap = (await context.poaps.get(
     createTokenID(event.params.tokenId, event.chainId),
   ))!;
   if (ev == null) {
     ev = {
       id: createEventID(event),
-      tokenCount: BigInt(0),
-      tokenMints: BigInt(0),
-      transferCount: BigInt(0),
+      poap_count: BigInt(0),
+      token_mints: BigInt(0),
+      transfer_count: BigInt(0),
       created: BigInt(event.block.timestamp),
-      chainId: event.chainId,
-    } as Event;
+      chain_id: event.chainId,
+    } as drops_stats;
   }
 
-  const updatedEvent: Event = {
+  const updatedEvent: drops_stats = {
     ...ev,
-    tokenCount: ev.tokenCount + BigInt(1),
-    tokenMints: ev.tokenMints + BigInt(1),
-    transferCount: ev.transferCount + BigInt(1),
+    poap_count: ev.poap_count + BigInt(1),
+    token_mints: ev.token_mints + BigInt(1),
+    transfer_count: ev.transfer_count + BigInt(1),
   };
 
-  const updatedToken: Token = {
-    ...token,
-    event_id: updatedEvent.id,
-    mintOrder: updatedEvent.tokenMints,
+  const updatedPoap: poaps = {
+    ...poap,
+    drop_id: updatedEvent.id,
+    mint_order: updatedEvent.token_mints,
   };
 
-  context.Event.set(updatedEvent);
-  context.Token.set(updatedToken);
+  context.drops_stats.set(updatedEvent);
+  context.poaps.set(updatedPoap);
 });
 
 AdminUpgradeabilityProxy.Transfer.handler(async ({ event, context }) => {
-  let token = await context.Token.get(
+  let poap = await context.poaps.get(
     createTokenID(event.params.tokenId, event.chainId),
   );
-  let from = await context.Account.get(
+  let from = await context.collectors.get(
     createAccountID(event.params.from, event.chainId),
   );
-  let to = await context.Account.get(
+  let to = await context.collectors.get(
     createAccountID(event.params.to, event.chainId),
   );
 
@@ -73,88 +73,88 @@ AdminUpgradeabilityProxy.Transfer.handler(async ({ event, context }) => {
       id: createAccountID(event.params.from, event.chainId),
       address: event.params.from,
       // The from account at least has to own one token
-      tokensOwned: BigInt(1),
-      chainId: event.chainId,
+      poaps_owned: BigInt(1),
+      chain_id: event.chainId,
     };
   }
   // Don't subtracts from the ZERO_ADDRESS (it's the one that mint the token)
   // Avoid negative values
-  const updatedFrom: Account = {
+  const updatedFrom: collectors = {
     ...from,
-    tokensOwned:
+    poaps_owned:
       from.address != ZERO_ADDRESS
-        ? from.tokensOwned - BigInt(1)
-        : from.tokensOwned,
+        ? from.poaps_owned - BigInt(1)
+        : from.poaps_owned,
   };
-  context.Account.set(updatedFrom);
+  context.collectors.set(updatedFrom);
 
   if (to == null) {
     to = {
       id: createAccountID(event.params.to, event.chainId),
       address: event.params.to,
-      tokensOwned: BigInt(0),
-      chainId: event.chainId,
+      poaps_owned: BigInt(0),
+      chain_id: event.chainId,
     };
   }
-  const updatedTo: Account = {
+  const updatedTo: collectors = {
     ...to,
-    tokensOwned: to.tokensOwned + BigInt(1),
+    poaps_owned: to.poaps_owned + BigInt(1),
   };
-  context.Account.set(updatedTo);
+  context.collectors.set(updatedTo);
 
-  if (token == null) {
-    token = {
+  if (poap == null) {
+    poap = {
       id: createTokenID(event.params.tokenId, event.chainId),
-      tokenId: event.params.tokenId,
-      transferCount: BigInt(0),
+      token_id: event.params.tokenId,
+      transfer_count: BigInt(0),
       created: BigInt(event.block.timestamp),
-      chainId: Number(event.chainId),
-    } as Token;
+      chain_id: Number(event.chainId),
+    } as poaps;
   }
 
-  const updatedToken: Token = {
-    ...token,
-    owner_id: updatedTo.id,
-    transferCount: token.transferCount + BigInt(1),
+  const updatedPoap: poaps = {
+    ...poap,
+    collector_address_id: updatedTo.id,
+    transfer_count: poap.transfer_count + BigInt(1),
   };
 
-  context.Token.set(updatedToken);
+  context.poaps.set(updatedPoap);
 
-  if (updatedToken.event_id != null) {
-    let evEntity = await context.Event.get(updatedToken.event_id as string);
+  if (updatedPoap.drop_id != null) {
+    let evEntity = await context.drops_stats.get(updatedPoap.drop_id as string);
 
     if (evEntity != null) {
       // Add one transfer
-      let updatedEvEntity: Event = {
+      let updatedEvEntity: drops_stats = {
         ...evEntity,
-        transferCount: evEntity.transferCount + BigInt(1),
+        transfer_count: evEntity.transfer_count + BigInt(1),
       };
 
       // Burning the token
       if (updatedTo.id == ZERO_ADDRESS) {
         updatedEvEntity = {
           ...updatedEvEntity,
-          tokenCount: updatedEvEntity.tokenCount - BigInt(1),
+          poap_count: updatedEvEntity.poap_count - BigInt(1),
           // Subtract all the transfers from the burned token
-          transferCount:
-            updatedEvEntity.transferCount - updatedToken.transferCount,
+          transfer_count:
+            updatedEvEntity.transfer_count - updatedPoap.transfer_count,
         };
       }
 
-      context.Event.set(updatedEvEntity);
+      context.drops_stats.set(updatedEvEntity);
     }
   }
 
-  const transfer: Transfer = {
+  const transfer: transfers = {
     id: createEventID(event),
-    chainId: event.chainId,
-    from_id: updatedFrom.id,
+    chain_id: event.chainId,
+    from_address_id: updatedFrom.id,
     timestamp: BigInt(event.block.timestamp),
-    blockNumber: BigInt(event.block.number),
-    to_id: updatedTo.id,
-    token_id: updatedToken.id,
+    block_number: BigInt(event.block.number),
+    to_address_id: updatedTo.id,
+    poap_id: updatedPoap.id,
     transaction: event.transaction.hash,
   };
 
-  context.Transfer.set(transfer);
+  context.transfers.set(transfer);
 });
